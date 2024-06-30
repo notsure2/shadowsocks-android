6db1df0d9e47479aa614b53cd32b56f5196263aa
notsure2 <notsure2@protonmail.com>
2024-06-30 21:53:54 +0300

    Fix workflow again

--- release.sh
+++ release.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+rm -rf release
+mkdir release
 release=$1
-cp mobile/build/outputs/apk/release/mobile-armeabi-v7a-release.apk         shadowsocks-armeabi-v7a-${release}.apk
-cp mobile/build/outputs/apk/release/mobile-arm64-v8a-release.apk           shadowsocks-arm64-v8a-${release}.apk
-cp mobile/build/outputs/apk/release/mobile-x86-release.apk                 shadowsocks-x86-${release}.apk
-cp mobile/build/outputs/apk/release/mobile-x86_64-release.apk              shadowsocks-x86_64-${release}.apk
-cp mobile/build/outputs/apk/release/mobile-universal-release.apk           shadowsocks--universal-${release}.apk
-cp tv/build/outputs/apk/freedom/release/tv-freedom-armeabi-v7a-release.apk shadowsocks-tv-armeabi-v7a-${release}.apk
-cp tv/build/outputs/apk/freedom/release/tv-freedom-arm64-v8a-release.apk   shadowsocks-tv-arm64-v8a-${release}.apk
-cp tv/build/outputs/apk/freedom/release/tv-freedom-x86-release.apk         shadowsocks-tv-x86-${release}.apk
-cp tv/build/outputs/apk/freedom/release/tv-freedom-x86_64-release.apk      shadowsocks-tv-x86_64-${release}.apk
-cp tv/build/outputs/apk/freedom/release/tv-freedom-universal-release.apk   shadowsocks-tv-universal-${release}.apk
+cp mobile/build/outputs/apk/release/mobile-armeabi-v7a-*.apk         release/shadowsocks-armeabi-v7a-${release}.apk
+cp mobile/build/outputs/apk/release/mobile-arm64-v8a-*.apk           release/shadowsocks-arm64-v8a-${release}.apk
+cp mobile/build/outputs/apk/release/mobile-x86-*.apk                 release/shadowsocks-x86-${release}.apk
+cp mobile/build/outputs/apk/release/mobile-x86_64-*.apk              release/shadowsocks-x86_64-${release}.apk
+cp mobile/build/outputs/apk/release/mobile-universal-*.apk           release/shadowsocks--universal-${release}.apk
+cp tv/build/outputs/apk/freedom/release/tv-freedom-armeabi-v7a-*.apk release/shadowsocks-tv-armeabi-v7a-${release}.apk
+cp tv/build/outputs/apk/freedom/release/tv-freedom-arm64-v8a-*.apk   release/shadowsocks-tv-arm64-v8a-${release}.apk
+cp tv/build/outputs/apk/freedom/release/tv-freedom-x86-*.apk         release/shadowsocks-tv-x86-${release}.apk
+cp tv/build/outputs/apk/freedom/release/tv-freedom-x86_64-*.apk      release/shadowsocks-tv-x86_64-${release}.apk
+cp tv/build/outputs/apk/freedom/release/tv-freedom-universal-*.apk   release/shadowsocks-tv-universal-${release}.apk
